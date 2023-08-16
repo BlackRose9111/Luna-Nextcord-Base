069c5db75918efb825e85ef7bdf87600df486114
Ömer Faruk BARAN <of123baran@outlook.com>
2023-08-16 12:03:16 +0300

    time manager

--- system/timemanager.py
+++ system/timemanager.py
@@ -1,2 +1,36 @@
+from datetime import datetime
+
 import pytz
+
+import main
+
+
+class Timemanager():
+    def __init__(self):
+        self.timezone = pytz.timezone(main.bot_time_zone)
+
+    def get_time(self):
+        return datetime.now(self.timezone)
+    def format_time(self,time):
+        #only hours and minutes
+        return time.strftime("%H:%M")
+    def format_seconds(self,seconds):
+        time = seconds
+        timesuffix = "S"
+        if seconds >= 60:
+            time = seconds / 60
+            timesuffix = "M"
+            if time >= 60:
+                time = time / 60
+                timesuffix = "H"
+                if time >= 24:
+                    time = time / 24
+                    timesuffix = "D"
+                    if time >= 365:
+                        time = time / 365
+                        timesuffix = "Y"
+        return str(round(time,2)) + timesuffix
+    def get_time_difference(self,first,second):
+        return (first - second).total_seconds()
+
 
